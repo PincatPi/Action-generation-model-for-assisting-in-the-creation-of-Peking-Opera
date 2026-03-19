0bc48c1e1b0e7e81237dbdbba0f02267b7f2483e
Yi Yang <126643613+PincatPi@users.noreply.github.com>
2026-03-20 02:29:54 +0800

    Add attention, filter and feeder tools

---
+++ VIBE/lib/models/attention.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import torch
+from torch import nn
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        torch.nn.init.uniform_(m.weight, -0.1, 0.1)
+        m.bias.data.fill_(0.01)
+
+class SelfAttention(nn.Module):
+    def __init__(self, attention_size,
+                 batch_first=False,
+                 layers=1,
+                 dropout=.0,
+                 non_linearity="tanh"):
+        super(SelfAttention, self).__init__()
+
+        self.batch_first = batch_first
+
+        if non_linearity == "relu":
+            activation = nn.ReLU()
+        else:
+            activation = nn.Tanh()
+
+        modules = []
+        for i in range(layers - 1):
+            modules.append(nn.Linear(attention_size, attention_size))
+            modules.append(activation)
+            modules.append(nn.Dropout(dropout))
+
+        modules.append(nn.Linear(attention_size, 1))
+        modules.append(activation)
+        modules.append(nn.Dropout(dropout))
+
+        self.attention = nn.Sequential(*modules)
+        self.attention.apply(init_weights) 
+        self.softmax = nn.Softmax(dim=-1)
+
+
+    def forward(self, inputs):
+        scores = self.attention(inputs).squeeze()
+        scores = self.softmax(scores)
+
+        weighted = torch.mul(inputs, scores.unsqueeze(-1).expand_as(inputs))
+
+        representations = weighted.sum(1).squeeze()
+        return representations, scores
