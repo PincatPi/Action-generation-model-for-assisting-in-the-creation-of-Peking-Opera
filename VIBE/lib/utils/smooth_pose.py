f13fff17e81a1ea4fd5a4cab8315795081e42c20
Yi Yang <126643613+PincatPi@users.noreply.github.com>
2026-03-20 03:20:06 +0800

    Add VIBE smooth pose and AMASS dataset

--- VIBE/lib/utils/smooth_pose.py
+++ VIBE/lib/utils/smooth_pose.py
@@ -30,12 +30,8 @@ def smooth_pose(pred_pose, pred_betas, min_cutoff=0.004, beta=0.7):
     pred_verts_hat.append(smpl_output.vertices.detach().cpu().numpy())
     pred_joints3d_hat.append(smpl_output.joints.detach().cpu().numpy())
 
-    for idx, pose in enumerate(pred_pose[1:]):
-        idx += 1
-
-        t = np.ones_like(pose) * idx
-        pose = one_euro_filter(t, pose)
-        pred_pose_hat[idx] = pose
+    for idx in range(1, pred_pose.shape[0]):
+        pred_pose_hat[idx], _ = one_euro_filter(pred_pose[idx], pred_pose[idx])
 
         smpl_output = smpl(
             betas=torch.from_numpy(pred_betas[idx]).unsqueeze(0),
@@ -45,25 +41,7 @@ def smooth_pose(pred_pose, pred_betas, min_cutoff=0.004, beta=0.7):
         pred_verts_hat.append(smpl_output.vertices.detach().cpu().numpy())
         pred_joints3d_hat.append(smpl_output.joints.detach().cpu().numpy())
 
-    return np.vstack(pred_verts_hat), pred_pose_hat, np.vstack(pred_joints3d_hat)
+    pred_verts_hat = np.concatenate(pred_verts_hat, axis=0)
+    pred_joints3d_hat = np.concatenate(pred_joints3d_hat, axis=0)
 
-
-def smooth_pose_and_cam(pred_pose, pred_betas, pred_cam, min_cutoff=0.004, beta=0.7):
-    pred_verts_hat, pred_pose_hat, pred_joints3d_hat = smooth_pose(
-        pred_pose, pred_betas, min_cutoff=min_cutoff, beta=beta
-    )
-
-    cam_filter = OneEuroFilter(
-        np.zeros_like(pred_cam[0]),
-        pred_cam[0],
-        min_cutoff=min_cutoff,
-        beta=beta,
-    )
-    pred_cam_hat = np.zeros_like(pred_cam)
-    pred_cam_hat[0] = pred_cam[0]
-
-    for idx in range(1, len(pred_cam)):
-        t = np.ones_like(pred_cam[0]) * idx
-        pred_cam_hat[idx] = cam_filter(t, pred_cam[idx])
-
-    return pred_verts_hat, pred_pose_hat, pred_joints3d_hat, pred_cam_hat
+    return pred_pose_hat, pred_verts_hat, pred_joints3d_hat
