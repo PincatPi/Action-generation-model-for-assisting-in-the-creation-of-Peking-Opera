43cadf80a76a784bf994090346a52c17bc027f5f
Yi Yang <126643613+PincatPi@users.noreply.github.com>
2026-03-20 02:58:54 +0800

    Add CTR_GCN main.py

---
+++ CTR_GCN/main.py
@@ -0,0 +1,187 @@
+from __future__ import print_function
+
+import argparse
+import inspect
+import os
+import pickle
+import random
+import shutil
+import sys
+import time
+from collections import OrderedDict
+import traceback
+from sklearn.metrics import confusion_matrix
+import csv
+import numpy as np
+import glob
+
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.optim as optim
+import yaml
+from tensorboardX import SummaryWriter
+from tqdm import tqdm
+
+from torchlight import DictAction
+
+
+try:
+    import resource
+    rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
+    resource.setrlimit(resource.RLIMIT_NOFILE, (2048, rlimit[1]))
+except ModuleNotFoundError:
+    pass
+
+def init_seed(seed):
+    torch.cuda.manual_seed_all(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+def import_class(import_str):
+    mod_str, _sep, class_str = import_str.rpartition('.')
+    __import__(mod_str)
+    try:
+        return getattr(sys.modules[mod_str], class_str)
+    except AttributeError:
+        raise ImportError('Class %s cannot be found (%s)' % (class_str, traceback.format_exception(*sys.exc_info())))
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Unsupported value encountered.')
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description='Spatial Temporal Graph Convolution Network')
+    parser.add_argument(
+        '--work-dir',
+        default='./work_dir/temp',
+        help='the work folder for storing results')
+
+    parser.add_argument('-model_saved_name', default='')
+    parser.add_argument(
+        '--config',
+        default='./config/nturgbd-cross-view/test_bone.yaml',
+        help='path to the configuration file')
+
+    parser.add_argument(
+        '--phase', default='train', help='must be train or test')
+    parser.add_argument(
+        '--save-score',
+        type=str2bool,
+        default=False,
+        help='if ture, the classification score will be stored')
+
+    parser.add_argument(
+        '--seed', type=int, default=1, help='random seed for pytorch')
+    parser.add_argument(
+        '--log-interval',
+        type=int,
+        default=100,
+        help='the interval for printing messages (#iteration)')
+    parser.add_argument(
+        '--save-interval',
+        type=int,
+        default=1,
+        help='the interval for storing models (#iteration)')
+    parser.add_argument(
+        '--save-epoch',
+        type=int,
+        default=30,
+        help='the start epoch to save model (#iteration)')
+    parser.add_argument(
+        '--eval-interval',
+        type=int,
+        default=5,
+        help='the interval for evaluating models (#iteration)')
+    parser.add_argument(
+        '--print-log',
+        type=str2bool,
+        default=True,
+        help='print logging or not')
+    parser.add_argument(
+        '--show-topk',
+        type=int,
+        default=[1, 5],
+        nargs='+',
+        help='which Top K accuracy will be shown')
+
+    parser.add_argument(
+        '--feeder', default='feeder.feeder', help='data loader will be used')
+    parser.add_argument(
+        '--num-worker',
+        type=int,
+        default=32,
+        help='the number of worker for data loader')
+    parser.add_argument(
+        '--train-feeder-args',
+        action=DictAction,
+        default=dict(),
+        help='the arguments of data loader for training')
+    parser.add_argument(
+        '--test-feeder-args',
+        action=DictAction,
+        default=dict(),
+        help='the arguments of data loader for test')
+
+    parser.add_argument('--model', default=None, help='the model will be used')
+    parser.add_argument(
+        '--model-args',
+        action=DictAction,
+        default=dict(),
+        help='the arguments of model')
+
+    return parser
+
+
+class Processor():
+    def __init__(self, arg):
+        self.arg = arg
+        self.save_arg()
+        if self.arg.phase == 'train':
+            pass
+
+    def save_arg(self):
+        pass
+
+    def print_info(self):
+        pass
+
+    def init_device(self):
+        pass
+
+    def load_model(self):
+        pass
+
+    def load_data(self):
+        pass
+
+    def train(self):
+        pass
+
+    def eval(self):
+        pass
+
+    def print_time(self):
+        pass
+
+    def record(self):
+        pass
+
+    def start(self):
+        pass
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    arg = parser.parse_args()
+    processor = Processor(arg)
+    processor.start()
