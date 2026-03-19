75c0c5eb05cb6a29bf30ec6169699b291493b681
Yi Yang <126643613+PincatPi@users.noreply.github.com>
2026-03-20 02:30:43 +0800

    Add image utils and UCLA graph

---
+++ VIBE/lib/data_utils/img_utils.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+
+import os
+import cv2
+import torch
+import random
+import numpy as np
+import torchvision.transforms as transforms
+from skimage.util.shape import view_as_windows
+
+def get_image(filename):
+    image = cv2.imread(filename)
+    return cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+def do_augmentation(scale_factor=0.3, color_factor=0.2):
+    scale = random.uniform(1.2, 1.2+scale_factor)
+    rot = 0
+    do_flip = False
+    c_up = 1.0 + color_factor
+    c_low = 1.0 - color_factor
+    color_scale = [random.uniform(c_low, c_up), random.uniform(c_low, c_up), random.uniform(c_low, c_up)]
+    return scale, rot, do_flip, color_scale
+
+def trans_point2d(pt_2d, trans):
+    src_pt = np.array([pt_2d[0], pt_2d[1], 1.]).T
+    dst_pt = np.dot(trans, src_pt)
+    return dst_pt[0:2]
+
+def rotate_2d(pt_2d, rot_rad):
+    x = pt_2d[0]
+    y = pt_2d[1]
+    sn, cs = np.sin(rot_rad), np.cos(rot_rad)
+    xx = x * cs - y * sn
+    yy = x * sn + y * cs
+    return np.array([xx, yy], dtype=np.float32)
+
+def gen_trans_from_patch_cv(c_x, c_y, src_width, src_height, dst_width, dst_height, scale, rot, inv=False):
+    src_w = src_width * scale
+    src_h = src_height * scale
+    src_center = np.zeros(2)
+    src_center[0] = c_x
+    src_center[1] = c_y
+    rot_rad = np.pi * rot / 180
+    src_downdir = rotate_2d(np.array([0, src_h * 0.5], dtype=np.float32), rot_rad)
+    src_rightdir = rotate_2d(np.array([src_w * 0.5, 0], dtype=np.float32), rot_rad)
+
+    dst_w = dst_width
+    dst_h = dst_height
+    dst_center = np.array([dst_w * 0.5, dst_h * 0.5], dtype=np.float32)
+    dst_downdir = np.array([0, dst_h * 0.5], dtype=np.float32)
+    dst_rightdir = np.array([dst_w * 0.5, 0], dtype=np.float32)
+
+    src = np.zeros((3, 2), dtype=np.float32)
+    src[0, :] = src_center
+    src[1, :] = src_center + src_downdir
+    src[2, :] = src_center + src_rightdir
+
+    dst = np.zeros((3, 2), dtype=np.float32)
+    dst[0, :] = dst_center
+    dst[1, :] = dst_center + dst_downdir
+    dst[2, :] = dst_center + dst_rightdir
+
+    if inv:
+        trans = cv2.getAffineTransform(np.float32(dst), np.float32(src))
+    else:
+        trans = cv2.getAffineTransform(np.float32(src), np.float32(dst))
+
+    return trans
+
+def generate_patch_image_cv(cvimg, c_x, c_y, bb_width, bb_height, patch_width, patch_height, do_flip, scale, rot):
+    img = cvimg.copy()
+    img_height, img_width, img_channels = img.shape
+
+    if do_flip:
+        img = img[:, ::-1, :]
+        c_x = img_width - c_x - 1
+
+    trans = gen_trans_from_patch_cv(c_x, c_y, bb_width, bb_height, patch_width, patch_height, scale, rot, inv=False)
+
+    img_patch = cv2.warpAffine(img, trans, (int(patch_width), int(patch_height)),
+                               flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+
+    return img_patch, trans
+
+def convert_cvimg_to_tensor(cvimg):
+    img = cvimg.copy()
+    img = img.astype(np.float32) / 255.
+    img = img[:, :, [2, 1, 0]]
+    img = img.transpose(2, 0, 1)
+    return torch.from_numpy(img).float()
+
+def get_single_image_crop_demo(img, bbox, kp_2d=None, scale=1.0, crop_size=224):
+    crop_image, trans = generate_patch_image_cv(
+        cvimg=img.copy(),
+        c_x=bbox[0],
+        c_y=bbox[1],
+        bb_width=bbox[2],
+        bb_height=bbox[3],
+        patch_width=crop_size,
+        patch_height=crop_size,
+        do_flip=False,
+        scale=scale,
+        rot=0,
+    )
+
+    if kp_2d is not None:
+        for n_jt in range(kp_2d.shape[0]):
+            kp_2d[n_jt] = trans_point2d(kp_2d[n_jt], trans)
+
+    crop_image = convert_cvimg_to_tensor(crop_image)
+    return crop_image, crop_image, kp_2d
