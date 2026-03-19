66de55d4b6154cd950ba5daaa577b61eaf27a310
Yi Yang <126643613+PincatPi@users.noreply.github.com>
2026-03-20 03:06:21 +0800

    Add inference dataset and kp_utils

--- VIBE/lib/data_utils/kp_utils.py
+++ VIBE/lib/data_utils/kp_utils.py
@@ -26,25 +26,88 @@ def get_perm_idxs(src, dst):
     return idxs
 
 def get_mpii3d_test_joint_names():
-    return ['headtop', 'neck', 'rshoulder', 'relbow', 'rwrist', 'lshoulder', 'lelbow', 'lwrist', 'rhip', 'rknee', 'rankle', 'lhip', 'lknee', 'lankle', 'hip', 'Spine (H36M)', 'Head (H36M)']
+    return [
+        'headtop', 'neck', 'rshoulder', 'relbow', 'rwrist',
+        'lshoulder', 'lelbow', 'lwrist', 'rhip', 'rknee', 'rankle',
+        'lhip', 'lknee', 'lankle', 'hip', 'Spine (H36M)', 'Head (H36M)',
+    ]
 
 def get_mpii3d_joint_names():
-    return ['spine3', 'spine4', 'spine2', 'Spine (H36M)', 'hip', 'neck', 'Head (H36M)', 'headtop', 'left_clavicle', 'lshoulder', 'lelbow', 'lwrist', 'left_hand', 'right_clavicle', 'rshoulder', 'relbow', 'rwrist', 'right_hand', 'lhip', 'lknee', 'lankle', 'left_foot', 'left_toe', 'rhip', 'rknee', 'rankle', 'right_foot', 'right_toe']
+    return [
+        'spine3', 'spine4', 'spine2', 'Spine (H36M)', 'hip', 'neck',
+        'Head (H36M)', "headtop", 'left_clavicle', "lshoulder", "lelbow",
+        "lwrist", 'left_hand', 'right_clavicle', 'rshoulder', 'relbow',
+        'rwrist', 'right_hand', 'lhip', 'lknee', 'lankle', 'left_foot',
+        'left_toe', "rhip", "rknee", "rankle", 'right_foot', 'right_toe'
+    ]
 
 def get_insta_joint_names():
-    return ['OP RHeel', 'OP RKnee', 'OP RHip', 'OP LHip', 'OP LKnee', 'OP LHeel', 'OP RWrist', 'OP RElbow', 'OP RShoulder', 'OP LShoulder', 'OP LElbow', 'OP LWrist', 'OP Neck', 'headtop', 'OP Nose', 'OP LEye', 'OP REye', 'OP LEar', 'OP REar', 'OP LBigToe', 'OP RBigToe', 'OP LSmallToe', 'OP RSmallToe', 'OP LAnkle', 'OP RAnkle']
-
-def get_spin_joint_names():
-    return ['OP Nose', 'OP Neck', 'OP RShoulder', 'OP RElbow', 'OP RWrist', 'OP LShoulder', 'OP LElbow', 'OP LWrist', 'OP MidHip', 'OP RHip', 'OP RKnee', 'OP RAnkle', 'OP LHip', 'OP LKnee', 'OP LAnkle', 'OP REye', 'OP LEye', 'OP REar', 'OP LEar', 'OP LBigToe', 'OP LSmallToe', 'OP RBigToe', 'OP RSmallToe', 'OP LHeel', 'OP RHeel', 'rankle', 'rknee', 'rhip', 'lhip', 'lknee', 'lankle', 'rwrist', 'relbow', 'rshoulder', 'lshoulder', 'lelbow', 'lwrist', 'neck', 'headtop', 'hip', 'Spine (H36M)', 'Head (H36M)', 'RShoulder', 'LShoulder', 'RElbow', 'LElbow', 'RWrist', 'LWrist', 'RHip', 'LHip', 'RKnee', 'LKnee', 'RAnkle', 'LAnkle']
-
-def get_common_joint_names():
-    return ['rankle', 'rknee', 'rhip', 'lhip', 'lknee', 'lankle', 'rwrist', 'relbow', 'rshoulder', 'lshoulder', 'lelbow', 'lwrist', 'neck', 'headtop']
-
-def get_h36m_joint_names():
-    return ['hip', 'lhip', 'lknee', 'lankle', 'rhip', 'rknee', 'rankle', 'Spine (H36M)', 'neck', 'headtop', 'lshoulder', 'lelbow', 'lwrist', 'rshoulder', 'relbow', 'rwrist']
+    return [
+        'OP Nose', 'OP Neck', 'OP RShoulder', 'OP RElbow', 'OP RWrist',
+        'OP LShoulder', 'OP LElbow', 'OP LWrist', 'OP MidHip',
+        'OP RHip', 'OP RKnee', 'OP RAnkle', 'OP LHip', 'OP LKnee',
+        'OP LAnkle', 'OP REye', 'OP LEye', 'OP REar', 'OP LEar',
+        'OP LBigToe', 'OP LSmallToe', 'OP LHeel', 'OP RBigToe',
+        'OP RSmallToe', 'OP RHeel',
+    ]
 
 def get_posetrack_joint_names():
-    return ['OP Nose', 'OP LEye', 'OP REye', 'OP LEar', 'OP REar', 'OP LShoulder', 'OP RShoulder', 'OP LElbow', 'OP RElbow', 'OP LWrist', 'OP RWrist', 'OP LHip', 'OP RHip', 'OP LKnee', 'OP RKnee', 'OP LAnkle', 'OP RAnkle']
+    return [
+        'OP Nose', 'OP Neck', 'OP RShoulder', 'OP RElbow', 'OP RWrist',
+        'OP LShoulder', 'OP LElbow', 'OP LWrist', 'OP MidHip',
+        'OP RHip', 'OP RKnee', 'OP RAnkle', 'OP LHip', 'OP LKnee',
+        'OP LAnkle', 'OP REye', 'OP LEye', 'OP REar', 'OP LEar',
+        'OP LBigToe', 'OP LSmallToe', 'OP LHeel', 'OP RBigToe',
+        'OP RSmallToe', 'OP RHeel',
+    ]
 
-def get_penn_action_joint_names():
-    return ['headtop', 'lshoulder', 'rshoulder', 'lelbow', 'relbow', 'lwrist', 'rwrist', 'lhip', 'rhip', 'lknee', 'rknee', 'lankle', 'rankle']
+def get_h36m_joint_names():
+    return [
+        'hip', 'lhip', 'lknee', 'lankle', 'rhip', 'rknee', 'rankle',
+        'Spine (H36M)', 'neck', 'Head (H36M)', 'headtop', 'lshoulder',
+        'lelbow', 'lwrist', 'rshoulder', 'relbow', 'rwrist',
+    ]
+
+def get_spin_joint_names():
+    return [
+        'OP Nose', 'OP Neck', 'OP RShoulder', 'OP RElbow', 'OP RWrist',
+        'OP LShoulder', 'OP LElbow', 'OP LWrist', 'OP MidHip',
+        'OP RHip', 'OP RKnee', 'OP RAnkle', 'OP LHip', 'OP LKnee',
+        'OP LAnkle', 'OP REye', 'OP LEye', 'OP REar', 'OP LEar',
+        'OP LBigToe', 'OP LSmallToe', 'OP LHeel', 'OP RBigToe',
+        'OP RSmallToe', 'OP RHeel', 'Right Ankle', 'Right Knee',
+        'Right Hip', 'Left Hip', 'Left Knee', 'Left Ankle', 'Right Wrist',
+        'Right Elbow', 'Right Shoulder', 'Left Shoulder', 'Left Elbow',
+        'Left Wrist', 'Neck (LSP)', 'Top of Head (LSP)', 'Pelvis (MPII)',
+        'Thorax (MPII)', 'Spine (H36M)', 'Jaw (H36M)', 'Head (H36M)',
+        'Nose', 'Left Eye', 'Right Eye', 'Left Ear', 'Right Ear'
+    ]
+
+def get_smpl_joint_names():
+    return [
+        'hips', 'leftUpLeg', 'leftLeg', 'leftFoot', 'leftToeBase',
+        'rightUpLeg', 'rightLeg', 'rightFoot', 'rightToeBase',
+        'spine', 'spine1', 'spine2', 'neck', 'head', 'leftShoulder',
+        'leftArm', 'leftForeArm', 'leftHand', 'rightShoulder',
+        'rightArm', 'rightForeArm', 'rightHand',
+    ]
+
+def get_openpose_joint_names():
+    return [
+        'OP Nose', 'OP Neck', 'OP RShoulder', 'OP RElbow', 'OP RWrist',
+        'OP LShoulder', 'OP LElbow', 'OP LWrist', 'OP MidHip',
+        'OP RHip', 'OP RKnee', 'OP RAnkle', 'OP LHip', 'OP LKnee',
+        'OP LAnkle', 'OP REye', 'OP LEye', 'OP REar', 'OP LEar',
+        'OP LBigToe', 'OP LSmallToe', 'OP LHeel', 'OP RBigToe',
+        'OP RSmallToe', 'OP RHeel',
+    ]
+
+def get_staf_joint_names():
+    return [
+        'OP Nose', 'OP Neck', 'OP RShoulder', 'OP RElbow', 'OP RWrist',
+        'OP LShoulder', 'OP LElbow', 'OP LWrist', 'OP MidHip',
+        'OP RHip', 'OP RKnee', 'OP RAnkle', 'OP LHip', 'OP LKnee',
+        'OP LAnkle', 'OP REye', 'OP LEye', 'OP REar', 'OP LEar',
+        'OP LBigToe', 'OP LSmallToe', 'OP LHeel', 'OP RBigToe',
+        'OP RSmallToe', 'OP RHeel',
+    ]
