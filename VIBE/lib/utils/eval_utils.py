9bd0864376bd6de9a5a610594e777dfaed4ef00a
Yi Yang <126643613+PincatPi@users.noreply.github.com>
2026-03-20 02:32:04 +0800

    Add eval and geometry utils

---
+++ VIBE/lib/utils/eval_utils.py
@@ -0,0 +1,159 @@
+import torch
+import numpy as np
+
+
+def compute_accel(joints):
+    velocities = joints[1:] - joints[:-1]
+    acceleration = velocities[1:] - velocities[:-1]
+    acceleration_normed = np.linalg.norm(acceleration, axis=2)
+    return np.mean(acceleration_normed, axis=1)
+
+
+def compute_error_accel(joints_gt, joints_pred, vis=None):
+    accel_gt = joints_gt[:-2] - 2 * joints_gt[1:-1] + joints_gt[2:]
+    accel_pred = joints_pred[:-2] - 2 * joints_pred[1:-1] + joints_pred[2:]
+
+    normed = np.linalg.norm(accel_pred - accel_gt, axis=2)
+
+    if vis is None:
+        new_vis = np.ones(len(normed), dtype=bool)
+    else:
+        invis = np.logical_not(vis)
+        invis1 = np.roll(invis, -1)
+        invis2 = np.roll(invis, -2)
+        new_invis = np.logical_or(invis, np.logical_or(invis1, invis2))[:-2]
+        new_vis = np.logical_not(new_invis)
+
+    return np.mean(normed[new_vis], axis=1)
+
+
+def compute_error_verts(pred_verts, target_verts=None, target_theta=None):
+    if target_verts is None:
+        from lib.models.smpl import SMPL_MODEL_DIR
+        from lib.models.smpl import SMPL
+        device = 'cpu'
+        smpl = SMPL(
+            SMPL_MODEL_DIR,
+            batch_size=1,
+        ).to(device)
+
+        betas = torch.from_numpy(target_theta[:,75:]).to(device)
+        pose = torch.from_numpy(target_theta[:,3:75]).to(device)
+
+        target_verts = []
+        b_ = torch.split(betas, 5000)
+        p_ = torch.split(pose, 5000)
+
+        for b,p in zip(b_,p_):
+            output = smpl(betas=b, body_pose=p[:, 3:], global_orient=p[:, :3], pose2rot=True)
+            target_verts.append(output.vertices.detach().cpu().numpy())
+
+        target_verts = np.concatenate(target_verts, axis=0)
+
+    assert len(pred_verts) == len(target_verts)
+    error_per_vert = np.sqrt(np.sum((target_verts - pred_verts) ** 2, axis=2))
+    return np.mean(error_per_vert, axis=1)
+
+
+def compute_similarity_transform(S1, S2):
+    transposed = False
+    if S1.shape[0] != 3 and S1.shape[0] != 2:
+        S1 = S1.T
+        S2 = S2.T
+        transposed = True
+    assert(S2.shape[1] == S1.shape[1])
+
+    mu1 = S1.mean(axis=1, keepdims=True)
+    mu2 = S2.mean(axis=1, keepdims=True)
+    X1 = S1 - mu1
+    X2 = S2 - mu2
+
+    var1 = np.sum(X1**2)
+
+    K = X1.dot(X2.T)
+
+    U, s, Vh = np.linalg.svd(K)
+    V = Vh.T
+    Z = np.eye(U.shape[0])
+    Z[-1, -1] *= np.sign(np.linalg.det(U.dot(V.T)))
+    R = V.dot(Z.dot(U.T))
+
+    scale = np.trace(R.dot(K)) / var1
+
+    t = mu2 - scale*(R.dot(mu1))
+
+    S1_hat = scale*R.dot(S1) + t
+
+    if transposed:
+        S1_hat = S1_hat.T
+
+    return S1_hat
+
+
+def compute_similarity_transform_torch(S1, S2):
+    transposed = False
+    if S1.shape[0] != 3 and S1.shape[0] != 2:
+        S1 = S1.T
+        S2 = S2.T
+        transposed = True
+    assert (S2.shape[1] == S1.shape[1])
+
+    mu1 = S1.mean(axis=1, keepdims=True)
+    mu2 = S2.mean(axis=1, keepdims=True)
+    X1 = S1 - mu1
+    X2 = S2 - mu2
+
+    var1 = torch.sum(X1 ** 2)
+
+    K = X1.mm(X2.T)
+
+    U, s, V = torch.svd(K)
+    Z = torch.eye(U.shape[0], device=S1.device)
+    Z[-1, -1] *= torch.sign(torch.det(U @ V.T))
+    R = V.mm(Z.mm(U.T))
+
+    scale = torch.trace(R.mm(K)) / var1
+    t = mu2 - scale * (R.mm(mu1))
+
+    S1_hat = scale * R.mm(S1) + t
+
+    if transposed:
+        S1_hat = S1_hat.T
+
+    return S1_hat
+
+
+def batch_compute_similarity_transform_torch(S1, S2):
+    transposed = False
+    if S1.shape[-1] != 3 and S1.shape[-1] != 2:
+        S1 = S1.permute(0, 2, 1)
+        S2 = S2.permute(0, 2, 1)
+        transposed = True
+
+    mu1 = S1.mean(axis=2, keepdims=True)
+    mu2 = S2.mean(axis=2, keepdims=True)
+
+    X1 = S1 - mu1
+    X2 = S2 - mu2
+
+    var1 = torch.sum(X1 ** 2, dim=1).sum(dim=1)
+
+    K = torch.bmm(X1, X2.permute(0, 2, 1))
+
+    U, s, V = torch.svd(K)
+    Z = torch.eye(U.shape[1], device=S1.device).unsqueeze(0)
+    Z = Z.repeat(U.shape[0], 1, 1)
+    Z[:, -1, -1] *= torch.sign(torch.det(torch.bmm(U, V.permute(0, 2, 1))))
+    R = torch.bmm(V, torch.bmm(Z, U.permute(0, 2, 1)))
+
+    var1 = var1.unsqueeze(-1)
+    scale = torch.einsum('bii->b', R.bmm(K)) / var1
+
+    t = mu2 - scale.unsqueeze(-1).unsqueeze(-1) * torch.bmm(R, mu1)
+
+    S1_hat = scale.unsqueeze(-1).unsqueeze(-1) * torch.bmm(R, S1) + t
+
+    if transposed:
+        S1_hat = S1_hat.permute(0, 2, 1)
+
+    return S1_hat
---
+++ VIBE/lib/utils/geometry.py
@@ -0,0 +1,171 @@
+# -*- coding: utf-8 -*-
+
+import torch
+import numpy as np
+from torch.nn import functional as F
+
+
+def batch_rodrigues(axisang):
+    axisang_norm = torch.norm(axisang + 1e-8, p=2, dim=1)
+    angle = torch.unsqueeze(axisang_norm, -1)
+    axisang_normalized = torch.div(axisang, angle)
+    angle = angle * 0.5
+    v_cos = torch.cos(angle)
+    v_sin = torch.sin(angle)
+    quat = torch.cat([v_cos, v_sin * axisang_normalized], dim=1)
+    rot_mat = quat2mat(quat)
+    rot_mat = rot_mat.view(rot_mat.shape[0], 9)
+    return rot_mat
+
+
+def quat2mat(quat):
+    norm_quat = quat
+    norm_quat = norm_quat / norm_quat.norm(p=2, dim=1, keepdim=True)
+    w, x, y, z = norm_quat[:, 0], norm_quat[:, 1], norm_quat[:, 2], norm_quat[:, 3]
+
+    batch_size = quat.size(0)
+
+    w2, x2, y2, z2 = w.pow(2), x.pow(2), y.pow(2), z.pow(2)
+    wx, wy, wz = w * x, w * y, w * z
+    xy, xz, yz = x * y, x * z, y * z
+
+    rotMat = torch.stack([
+        w2 + x2 - y2 - z2, 2 * xy - 2 * wz, 2 * wy + 2 * xz, 2 * wz + 2 * xy,
+        w2 - x2 + y2 - z2, 2 * yz - 2 * wx, 2 * xz - 2 * wy, 2 * wx + 2 * yz,
+        w2 - x2 - y2 + z2
+    ], dim=1).view(batch_size, 3, 3)
+    return rotMat
+
+
+def rotation_matrix_to_angle_axis(rotation_matrix):
+    if rotation_matrix.shape[1:] == (3,3):
+        rot_mat = rotation_matrix.reshape(-1, 3, 3)
+        hom = torch.tensor([0, 0, 1], dtype=torch.float32,
+                           device=rotation_matrix.device).reshape(1, 3, 1).expand(rot_mat.shape[0], -1, -1)
+        rotation_matrix = torch.cat([rot_mat, hom], dim=-1)
+
+    quaternion = rotation_matrix_to_quaternion(rotation_matrix)
+    aa = quaternion_to_angle_axis(quaternion)
+    aa[torch.isnan(aa)] = 0.0
+    return aa
+
+
+def quaternion_to_angle_axis(quaternion: torch.Tensor) -> torch.Tensor:
+    if not torch.is_tensor(quaternion):
+        raise TypeError("Input type is not a torch.Tensor. Got {}".format(type(quaternion)))
+
+    if not quaternion.shape[-1] == 4:
+        raise ValueError("Input must be a tensor of shape Nx4 or 4. Got {}".format(quaternion.shape))
+
+    q1: torch.Tensor = quaternion[..., 1]
+    q2: torch.Tensor = quaternion[..., 2]
+    q3: torch.Tensor = quaternion[..., 3]
+    sin_squared_theta: torch.Tensor = q1 * q1 + q2 * q2 + q3 * q3
+
+    sin_theta: torch.Tensor = torch.sqrt(sin_squared_theta)
+    cos_theta: torch.Tensor = quaternion[..., 0]
+    two_theta: torch.Tensor = 2.0 * torch.where(
+        cos_theta < 0.0,
+        torch.atan2(-sin_theta, -cos_theta),
+        torch.atan2(sin_theta, cos_theta))
+
+    k_pos: torch.Tensor = two_theta / sin_theta
+    k_neg: torch.Tensor = 2.0 * torch.ones_like(sin_theta)
+    k: torch.Tensor = torch.where(sin_squared_theta > 0.0, k_pos, k_neg)
+
+    angle_axis: torch.Tensor = torch.zeros_like(quaternion)[..., :3]
+    angle_axis[..., 0] += q1 * k
+    angle_axis[..., 1] += q2 * k
+    angle_axis[..., 2] += q3 * k
+    return angle_axis
+
+
+def rotation_matrix_to_quaternion(rotation_matrix, eps=1e-6):
+    if not torch.is_tensor(rotation_matrix):
+        raise TypeError("Input type is not a torch.Tensor. Got {}".format(type(rotation_matrix)))
+
+    if len(rotation_matrix.shape) > 3:
+        raise ValueError("Input size must be a three dimensional tensor. Got {}".format(rotation_matrix.shape))
+    if not rotation_matrix.shape[-2:] == (3, 4):
+        raise ValueError("Input size must be a N x 3 x 4  tensor. Got {}".format(rotation_matrix.shape))
+
+    rmat_t = torch.transpose(rotation_matrix, 1, 2)
+
+    mask_d2 = rmat_t[:, 2, 2] < eps
+
+    mask_d0_d1 = rmat_t[:, 0, 0] > rmat_t[:, 1, 1]
+    mask_d0_nd1 = rmat_t[:, 0, 0] < -rmat_t[:, 1, 1]
+
+    t0 = 1 + rmat_t[:, 0, 0] - rmat_t[:, 1, 1] - rmat_t[:, 2, 2]
+    q0 = torch.stack([rmat_t[:, 1, 2] - rmat_t[:, 2, 1], t0, rmat_t[:, 0, 1] + rmat_t[:, 1, 0],
+                      rmat_t[:, 2, 0] + rmat_t[:, 0, 2]], -1)
+    t0_rep = t0.repeat(4, 1).t()
+
+    t1 = 1 - rmat_t[:, 0, 0] + rmat_t[:, 1, 1] - rmat_t[:, 2, 2]
+    q1 = torch.stack([rmat_t[:, 2, 0] - rmat_t[:, 0, 2], rmat_t[:, 0, 1] + rmat_t[:, 1, 0],
+                      t1, rmat_t[:, 1, 2] + rmat_t[:, 2, 1]], -1)
+    t1_rep = t1.repeat(4, 1).t()
+
+    t2 = 1 - rmat_t[:, 0, 0] - rmat_t[:, 1, 1] + rmat_t[:, 2, 2]
+    q2 = torch.stack([rmat_t[:, 0, 1] - rmat_t[:, 1, 0], rmat_t[:, 2, 0] + rmat_t[:, 0, 2],
+                      rmat_t[:, 1, 2] + rmat_t[:, 2, 1], t2], -1)
+    t2_rep = t2.repeat(4, 1).t()
+
+    t3 = 1 + rmat_t[:, 0, 0] + rmat_t[:, 1, 1] + rmat_t[:, 2, 2]
+    q3 = torch.stack([t3, rmat_t[:, 1, 2] - rmat_t[:, 2, 1], rmat_t[:, 2, 0] - rmat_t[:, 0, 2],
+                      rmat_t[:, 0, 1] - rmat_t[:, 1, 0]], -1)
+    t3_rep = t3.repeat(4, 1).t()
+
+    mask_c0 = mask_d2 * mask_d0_d1
+    mask_c1 = mask_d2 * ~mask_d0_d1
+    mask_c2 = ~mask_d2 * mask_d0_nd1
+    mask_c3 = ~mask_d2 * ~mask_d0_nd1
+    mask_c0 = mask_c0.view(-1, 1).type_as(q0)
+    mask_c1 = mask_c1.view(-1, 1).type_as(q1)
+    mask_c2 = mask_c2.view(-1, 1).type_as(q2)
+    mask_c3 = mask_c3.view(-1, 1).type_as(q3)
+
+    q = q0 * mask_c0 + q1 * mask_c1 + q2 * mask_c2 + q3 * mask_c3
+    q /= torch.sqrt(t0_rep * mask_c0 + t1_rep * mask_c1 + t2_rep * mask_c2 + t3_rep * mask_c3)
+    q *= 0.5
+    return q
+
+
+def rot6d_to_rotmat(x):
+    x = x.view(-1,3,2)
+
+    b1 = F.normalize(x[:, :, 0], dim=1, eps=1e-6)
+
+    dot_prod = torch.sum(b1 * x[:, :, 1], dim=1, keepdim=True)
+    b2 = F.normalize(x[:, :, 1] - dot_prod * b1, dim=-1, eps=1e-6)
+
+    b3 = torch.cross(b1, b2, dim=1)
+    rot_mats = torch.stack([b1, b2, b3], dim=-1)
+
+    return rot_mats
+
+
+def projection(pred_joints, pred_camera):
+    pred_cam_t = torch.stack([pred_camera[:, 1],
+                              pred_camera[:, 2],
+                              2 * 5000. / (224. * pred_camera[:, 0] + 1e-9)], dim=-1)
+    batch_size = pred_joints.shape[0]
+    camera_center = torch.zeros(batch_size, 2)
+    pred_keypoints_2d = weak_perspective_projection(
+        pred_joints, pred_cam_t, camera_center, 5000
+    )
+    return pred_keypoints_2d
+
+
+def weak_perspective_projection(points, translation, focal_length, camera_center):
+    batch_size = points.shape[0]
+    K = torch.zeros([batch_size, 3, 3], device=points.device)
+    K[:, 0, 0] = focal_length
+    K[:, 1, 1] = focal_length
+    K[:, 2, 2] = 1.
+    K[:, :-1, -1] = camera_center
+
+    points = points + translation.unsqueeze(1)
+    points = torch.einsum('bij,bkj->bik', points, K)
+    points = points[:, :, :-1] / points[:, :, -1:].unsqueeze(-1)
+    return points
