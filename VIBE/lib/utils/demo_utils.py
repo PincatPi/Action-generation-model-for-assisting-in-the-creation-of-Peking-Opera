a154f39393ee113e0b578ba1bc28548f383fce3d
Yi Yang <126643613+PincatPi@users.noreply.github.com>
2026-03-20 03:28:18 +0800

    Add VIBE demo and eval utils

--- VIBE/lib/utils/demo_utils.py
+++ VIBE/lib/utils/demo_utils.py
@@ -1,18 +1,12 @@
 import os
 import cv2
-import time
-import json
 import torch
-import subprocess
 import numpy as np
 import os.path as osp
-from pytube import YouTube
 from collections import OrderedDict
 
 from lib.utils.smooth_bbox import get_smooth_bbox_params, get_all_bbox_params
 from lib.data_utils.img_utils import get_single_image_crop_demo
-from lib.utils.geometry import rotation_matrix_to_angle_axis
-from lib.smplify.temporal_smplify import TemporalSMPLify
 
 
 def preprocess_video(video, joints2d, bboxes, frames, scale=1.0, crop_size=224):
@@ -30,125 +24,52 @@ def preprocess_video(video, joints2d, bboxes, frames, scale=1.0, crop_size=224):
     temp_video = np.zeros((shape[0], crop_size, crop_size, shape[-1]))
     norm_video = torch.zeros(shape[0], shape[-1], crop_size, crop_size)
 
-    for idx in range(video.shape[0]):
+    for idx in range(shape[0]):
         img = video[idx]
         bbox = bboxes[idx]
 
-        j2d = joints2d[idx] if joints2d is not None else None
-
-        norm_img, raw_img, kp_2d = get_single_image_crop_demo(
+        norm_img, _ = get_single_image_crop_demo(
             img,
             bbox,
-            kp_2d=j2d,
+            kp_2d=joints2d[idx] if joints2d is not None else None,
             scale=scale,
             crop_size=crop_size)
 
-        if joints2d is not None:
-            joints2d[idx] = kp_2d
-
-        temp_video[idx] = raw_img
-        norm_video[idx] = norm_img
-
-    temp_video = temp_video.astype(np.uint8)
+        temp_video[idx] = norm_img
+        norm_video[idx] = torch.from_numpy(norm_img.astype(np.float32)).permute(2,0,1) / 255.
 
     return temp_video, norm_video, bboxes, joints2d, frames
 
 
-def download_youtube_clip(url, download_folder):
-    return YouTube(url).streams.first().download(output_path=download_folder)
-
-
-def smplify_runner(pred_rotmat, pred_betas, pred_cam, j2d, device, batch_size, lr=1.0, opt_steps=1, use_lbfgs=True, pose2aa=True):
-    smplify = TemporalSMPLify(
-        step_size=lr,
-        batch_size=batch_size,
-        num_iters=opt_steps,
-        focal_length=5000.,
-        use_lbfgs=use_lbfgs,
-        device=device,
-    )
-    if pose2aa:
-        pred_pose = rotation_matrix_to_angle_axis(pred_rotmat.detach()).reshape(batch_size, -1)
-    else:
-        pred_pose = pred_rotmat
-
-    pred_cam_t = torch.stack([
-        pred_cam[:, 1], pred_cam[:, 2],
-        2 * 5000 / (224 * pred_cam[:, 0] + 1e-9)
-    ], dim=-1)
-
-    gt_keypoints_2d_orig = j2d
-    opt_joint_loss = smplify.get_fitting_loss(
-        pred_pose.detach(), pred_betas.detach(),
-        pred_cam_t.detach(),
-        0.5 * 224 * torch.ones(batch_size, 2, device=device),
-        gt_keypoints_2d_orig).mean(dim=-1)
-
-    best_prediction_id = torch.argmin(opt_joint_loss).item()
-    pred_betas = pred_betas[best_prediction_id].unsqueeze(0)
-
-    start = time.time()
-    output, new_opt_joint_loss = smplify(
-        pred_pose.detach(), pred_betas.detach(),
-        pred_cam_t.detach(),
-        0.5 * 224 * torch.ones(batch_size, 2, device=device),
-        gt_keypoints_2d_orig,
-    )
-    new_opt_joint_loss = new_opt_joint_loss.mean(dim=-1)
-
-    return output
-
-
-def convert_crop_coords_to_orig_img(bbox, keypoints, crop_size=224):
-    for idx in range(keypoints.shape[0]):
-        keypoints[idx, :, :2] = keypoints[idx, :, :2] / crop_size * bbox[idx, 2]
-        keypoints[idx, :, 0] += bbox[idx, 0] - bbox[idx, 2] / 2
-        keypoints[idx, :, 1] += bbox[idx, 1] - bbox[idx, 3] / 2
-    return keypoints
-
-
-def convert_crop_cam_to_orig_img(cam, bbox, crop_size=224, orig_shape=None):
-    scale = bbox[..., 2] / crop_size
-    cam_cx = bbox[..., 0] - bbox[..., 2] / 2
-    cam_cy = bbox[..., 1] - bbox[..., 3] / 2
-    orig_cam = np.stack([cam[:, 0] / scale, cam[:, 1] + cam_cx, cam[:, 2] + cam_cy], axis=-1)
-    return orig_cam
-
-
-def prepare_rendering_results(vibe_results, frame_count):
-    vertices = []
-    cam = []
-    for person_id in vibe_results.keys():
-        vertices.append(vibe_results[person_id]['verts'])
-        cam.append(vibe_results[person_id]['pred_cam'])
-
-    vertices = np.concatenate(vertices, axis=0)
-    cam = np.concatenate(cam, axis=0)
-
-    return vertices, cam
-
-
-def video_to_images(video_file, output_folder=None, return_info=False):
+def video_to_images(video_file):
     cap = cv2.VideoCapture(video_file)
-    frames = []
-    while cap.isOpened():
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    image_folder = video_file.replace('.mp4', '')
+    os.makedirs(image_folder, exist_ok=True)
+
+    for i in range(frame_count):
         ret, frame = cap.read()
-        if ret:
-            frames.append(frame)
-        else:
+        if not ret:
             break
+        cv2.imwrite(osp.join(image_folder, f'{i:06d}.jpg'), frame)
+
     cap.release()
-    return frames
+    return image_folder, frame_count, (height, width)
 
 
-def images_to_video(images, output_file, fps=30):
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    h, w = images[0].shape[:2]
-    out = cv2.VideoWriter(output_file, fourcc, fps, (w, h))
-    for img in images:
-        out.write(img)
-    out.release()
+def images_to_video(image_folder, output_file, fps=30):
+    images = sorted([img for img in os.listdir(image_folder) if img.endswith('.png') or img.endswith('.jpg')])
 
+    frame = cv2.imread(osp.join(image_folder, images[0]))
+    height, width, layers = frame.shape
 
-def download_ckpt(use_3dpw=False):
-    return 'data/vibe_model_w_3dpw.pth.tar' if use_3dpw else 'data/vibe_model_wo_3dpw.pth.tar'
+    video = cv2.VideoWriter(output_file, cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
+
+    for image in images:
+        video.write(cv2.imread(osp.join(image_folder, image)))
+
+    cv2.destroyAllWindows()
+    video.release()
--- VIBE/lib/utils/eval_utils.py
+++ VIBE/lib/utils/eval_utils.py
@@ -28,26 +28,15 @@ def compute_error_accel(joints_gt, joints_pred, vis=None):
 
 
 def compute_error_verts(pred_verts, target_verts=None, target_theta=None):
+    if target_verts is None and target_theta is None:
+        raise ValueError('Either target_verts or target_theta must be provided')
+
     if target_verts is None:
-        from lib.models.smpl import SMPL_MODEL_DIR
         from lib.models.smpl import SMPL
-        device = 'cpu'
-        smpl = SMPL(SMPL_MODEL_DIR, batch_size=1).to(device)
+        smpl = SMPL()
+        target_verts = smpl(torch.tensor(target_theta))
 
-        betas = torch.from_numpy(target_theta[:,75:]).to(device)
-        pose = torch.from_numpy(target_theta[:,3:75]).to(device)
-
-        target_verts = []
-        b_ = torch.split(betas, 5000)
-        p_ = torch.split(pose, 5000)
-
-        for b,p in zip(b_,p_):
-            output = smpl(betas=b, body_pose=p[:, 3:], global_orient=p[:, :3], pose2rot=True)
-            target_verts.append(output.vertices.detach().cpu().numpy())
-
-        target_verts = np.concatenate(target_verts, axis=0)
-
-    error = np.mean(np.linalg.norm(pred_verts - target_verts, axis=-1))
+    error = np.mean(np.linalg.norm(pred_verts - target_verts, axis=2), axis=1)
     return error
 
 
@@ -59,38 +48,17 @@ def batch_compute_similarity_transform_torch(S1, S2):
     mu1 = S1.mean(dim=2, keepdim=True)
     mu2 = S2.mean(dim=2, keepdim=True)
 
-    X1 = S1 - mu1
-    X2 = S2 - mu2
+    S1 = S1 - mu1
+    S2 = S2 - mu2
 
-    var1 = torch.sum(X1**2, dim=2).sum(dim=1)
-    std1 = torch.sqrt(var1 + 1e-6)
+    H = torch.bmm(S1, S2.permute(0, 2, 1))
+    U, s, V = torch.svd(H)
 
-    X1 = X1 / std1.unsqueeze(-1).unsqueeze(-1)
-    X2 = X2 / std1.unsqueeze(-1).unsqueeze(-1)
+    R = torch.bmm(V, U.permute(0, 2, 1))
 
-    R = torch.matmul(X1, X2.permute(0, 2, 1))
-    R = R.cpu().numpy()
+    var1 = torch.sum(S1 ** 2, dim=(1, 2))
+    scale = torch.sum(s, dim=1) / var1
 
-    S1 = S1.cpu().numpy()
-    S2 = S2.cpu().numpy()
+    t = mu2 - scale.view(-1, 1, 1) * torch.bmm(R, mu1)
 
-    out = []
-    for i in range(batch_size):
-        U, s, Vt = np.linalg.svd(R[i])
-        r = np.dot(U, Vt)
-        s = np.linalg.det(r)
-        s = np.diag([1, 1, s])
-        r = np.dot(U, np.dot(s, Vt))
-        out.append(r)
-
-    R = np.stack(out, axis=0)
-    R = torch.from_numpy(R).float().cuda()
-
-    S1 = torch.from_numpy(S1).float().cuda()
-    S2 = torch.from_numpy(S2).float().cuda()
-
-    S1 = torch.matmul(R, S1)
-
-    S1 = S1.permute(0, 2, 1)
-
-    return S1
+    return R, scale, t
